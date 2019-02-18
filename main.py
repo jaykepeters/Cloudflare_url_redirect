27867e1a331f6c42fc3f2109d1bf90e3ae76863b
Jayke Peters <jaykepeters@gmail.com>
2019-02-18 01:47:44 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from validator_collection import checkers
 cf = CloudFlare.CloudFlare(email='', token='')
 class redirector(object):
     def get_zone_id(self, domain):
-        zones = cf.zones.get(params={'per_page': 100, 'name': domain'})
+        zones = cf.zones.get(params={'per_page': 100, 'name': domain})
         for zone in zones:
             zone_id = zone['id']
             zone_name = zone['name']
